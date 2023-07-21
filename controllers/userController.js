c397826d37a680987f4e27ae7fdb3d8aa4d94ed0
İsmail ASLAN <1aslan1ismail@gmail.com>
2023-07-21 23:22:30 +0200

    add initial create and delete user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,62 @@
+const bcrypt = require("bcrypt");
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
+const { User } = require("../models");
 
 exports.createUser = catchAsync(async (req, res, next) => {
-  // if mail is already registered
-  return next(new AppError("This email has alredy been taken!", 400));
-  // await User.create(req.user.id, { active: false });
-  // res.status(204).send({
-  //     status: 'success',
-  //     data: null
-  // });
+  const { name, surname, email, password } = req.body;
+  console.log({
+    name,
+    surname,
+    email,
+    password,
+  });
+  if (
+    !name ||
+    name.trim() === "" ||
+    !surname ||
+    surname.trim() === "" ||
+    !email ||
+    email.trim() === "" ||
+    !password ||
+    password.trim() === ""
+  ) {
+    return next(new AppError("Missing data", 400));
+  }
+  const users = await User.findAll({
+    where: {
+      email: email,
+    },
+  });
+
+  if (users.length === 0) {
+    const newUser = await User.create({
+      name,
+      surname,
+      password: bcrypt.hashSync(password, 8),
+      email,
+    });
+
+    res.status(200).send({
+      status: "success",
+      data: {
+        name: newUser.name,
+        surname: newUser.surname,
+        email: newUser.email,
+      },
+    });
+  } else {
+    return next(new AppError("This email has already been registered.", 400));
+  }
+});
+
+exports.deleteUserById = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  await User.destroy({
+    where: { id },
+  });
+
+  res.status(204).send({
+    status: "success",
+  });
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const userController = require("./../controllers/userController");
 
-router.post("/register", userController.createUser);
+router
+  .post("/register", userController.createUser)
+  .delete("/:id", userController.deleteUserById);
 
 module.exports = router;
