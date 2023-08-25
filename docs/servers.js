457e240acae59294cf4c61e2318b4a2683e86bb7
İsmail ASLAN <1aslan1ismail@gmail.com>
2023-08-25 23:06:10 +0200

    docs server update

--- docs/servers.js
+++ docs/servers.js
@@ -1,6 +1,10 @@
 module.exports = {
   servers: [
     {
+      url: "https://lapi-ecommerce.ismailaslan.me/api/v1",
+      description: "Production server",
+    },
+    {
       url: "http://localhost:8000/api/v1",
       description: "Local server",
     },
